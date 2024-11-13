1946f628824cec91eef146199a1d71c03a96268a
Doby Lanete <112019683+dolanete@users.noreply.github.com>
2024-11-13 13:48:34 -0600

    Bot base
    Added the base of the chatbot

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # discordbot
+
+Usage of pip install discord and pip install python-dotenv
---
+++ responses.py
@@ -0,0 +1,13 @@
+from random import choice, randint
+
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'Well, you\'re awfully silent...'
+    elif 'hello' in lowered:
+        return "hello!!!"
+    elif 'roll dice' in lowered:
+        return  f'You rolled: {randint(1,6)}'
+    else:
+        return "What?"
